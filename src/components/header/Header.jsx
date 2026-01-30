02f8cdf618ec436329a6915cb5cc643959a4390f
jefersonssant <jefss@outlook.com>
2026-01-30 19:37:46 -0300

    :lipstick: feat: Estilo do header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,31 @@
-import "./header.module.scss"
+import s from "./header.module.scss"
 import { Link } from "react-router-dom"
 import logo from "../../assets/icones/logo.png"
 
 const Header = () => {
   return (
-    <header>
-      <img src={logo} alt="Imagem de logo do projeto connect sendo representada por uma mão segurando um coração, simbolizando apoio" />
+    <>
+      <header className={s.header}>
+        <Link to={"/"}>
+          <img className={s.imgLogo} src={logo} alt="Imagem de logo do projeto connect sendo representada por uma mão segurando um coração, simbolizando apoio" />
+        </Link>
 
-      <nav>
-        <Link to={"/doacao"}>Doação</Link>
-        <Link to={"/voluntariado"}>Voluntariado</Link>
-        <Link to={"/mentoria"}>Mentoria</Link>
-        <Link to={"/eventosEP"}>Eventos</Link>
+        <nav className={s.nav}>
+          <Link className={s.Link} to={"/doacao"}>Doação</Link>
+          <Link className={s.Link} to={"/voluntariado"}>Voluntariado</Link>
+          <Link className={s.Link} to={"/mentoria"}>Mentoria</Link>
+          <Link className={s.Link} to={"/eventosEP"}>Eventos</Link>
+        </nav>
+
+        <img className={s.imgUser} src="https://avatars.githubusercontent.com/u/133176621?v=4" alt="Imagem do usuário" />
+      </header>
+      <nav className={s.navUser}>
+        <Link>Jeferson Santos</Link>
+        <Link>Meu voluntariado</Link>
+        <Link>Configurações da conta</Link>
+        <Link>Sair</Link>
       </nav>
-
-      <img src="https://avatars.githubusercontent.com/u/133176621?v=4" alt="Imagem do usuário" />
-    </header>
+    </>
   )
 }
 
